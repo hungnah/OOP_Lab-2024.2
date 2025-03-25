c2181bff57f644bc6cd52e68290e6b0d0a53bd76
hungnah <nxthanhhung@gmail.com>
2025-03-25 16:18:07 +0700

    Thông điệp mô tả các thay đổi bạn đã thực hiện

--- W3/calculator.java
+++ W3/calculator.java
@@ -1,32 +1,23 @@
 import java.util.Scanner;
 
 public class calculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Nhập số thứ nhất: ");
-        String strNum1 = scanner.nextLine();
-        double num1 = Double.parseDouble(strNum1);
-
-        System.out.print("Nhập số thứ hai: ");
-        String strNum2 = scanner.nextLine();
-        double num2 = Double.parseDouble(strNum2);
-
-        double sum = num1 + num2;
-        double difference = num1 - num2;
-        double product = num1 * num2;
-
-        System.out.println("Tổng: " + sum);
-        System.out.println("Hiệu: " + difference);
-        System.out.println("Tích: " + product);
-
-        if (num2 != 0) {
-            double quotient = num1 / num2;
-            System.out.println("Thương: " + quotient);
-        } else {
-            System.out.println("Không thể chia cho 0!");
-        }
-
-        scanner.close();
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Nhap so thu nhat : ");
+		double num1 = scanner.nextDouble();
+		
+		System.out.print("Nhap so thu hai : ");
+		double num2 = scanner.nextDouble();
+		
+		double tong = num1 + num2 ;
+		double hieu = num1 - num2 ;
+		double tich = num1 * num2 ;
+		double thuong =(num1/num2);
+		
+		System.out.println("Tong la : " + tong);
+		System.out.println("Hieu la : " + hieu);
+		System.out.println("Tich la : " + tich);
+		System.out.println("Thuong la : " + thuong);
+	}
 }
